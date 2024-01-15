8fd8d2061a8d8223906861f3a4a19785030cda50
salman-alvi2k <102091911+salman-alvi2k@users.noreply.github.com>
2024-01-15 20:31:54 +0500

    feat: setup marker.io

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
+import React, { useEffect } from "react";
 import "./App.css";
-import Appbar from "./Components/Appbar/Appbar";
+
 import Header from "./Components/Header/FirstHeader";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Container } from "@mui/system";
 import Home from "./Components/Home/Home";
+import markerSDK from "@marker.io/browser";
 
 function App() {
+  useEffect(() => {
+    const setupMarkerIO = async () => {
+      const widget = await markerSDK.loadWidget({ project: "abcd1234567890" });
+      widget.show();
+    };
+
+    setupMarkerIO();
+  }, []);
+
   return (
-      <Container sx={{
-        paddings: 'none',
-        margin: 'none',
-      }}>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/Header" element={<Header />} />
-          </Routes>
-        </Router>
-      </Container>
-     
+    <Container
+      sx={{
+        paddings: "none",
+        margin: "none",
+      }}
+    >
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Header" element={<Header />} />
+        </Routes>
+      </Router>
+    </Container>
   );
 }
 
